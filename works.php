c18e15355a974a00e20077d065220a66dd6c72d5
Hana <happy_bian_cam_cam_123@yahoo.co.jp>
2021-08-01 21:32:18 +0900

    js追加しました。
    ふわっと表示

--- works.php
+++ works.php
@@ -23,26 +23,26 @@ Template Name: works
             </ul><!-- nav-list -->
 
                 <section class="archive-wrapper">
-                    <h3 class="main-ttl">my work</h3>
-                    <h4 class="list-ttl">制作実績を紹介します。</h4>
+                    <h3 class="main-ttl fadein">my work</h3>
+                    <h4 class="list-ttl fadein">制作実績を紹介します。</h4>
 
                     <div class="works-img">
 
-                        <a class="img-filter" href="<?php echo get_permalink(9); ?>">
+                        <a class="img-filter fadein" href="<?php echo get_permalink(9); ?>">
                             <img src="<?php echo get_template_directory_uri(); ?>/imge/portoforio01-img.png">
                             <div class="img-mask">
                                 <div class="img-cap">portoforio-2020</div>
                             </div>
                         </a><!-- img-filter -->
 
-                        <a class="img-filter" href="<?php echo get_permalink(11); ?>">
+                        <a class="img-filter fadein" href="<?php echo get_permalink(11); ?>">
                             <img src="<?php echo get_template_directory_uri(); ?>/imge/portoforio2021-img.png">
                             <div class="img-mask">
                                 <div class="img-cap">portoforio-2021</div>
                             </div>
                         </a><!-- img-filter -->
 
-                        <a class="img-filter" href="<?php echo get_permalink(14); ?>">
+                        <a class="img-filter fadein" href="<?php echo get_permalink(14); ?>">
                             <img src="<?php echo get_template_directory_uri(); ?>/imge/kurisuta-img.png">
                             <div class="img-mask">
                                 <div class="img-cap">kurisuta</div>
